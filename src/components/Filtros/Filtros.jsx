f0a47d7381a06bcdc12b939901b3ed6135f62fc4
marcoshjunior <155772166+marcoshjunior@users.noreply.github.com>
2026-09-01 19:15:45 -0300

    estilizando os botões top animes e animes da temporada

--- src/components/Filtros/Filtros.jsx
+++ src/components/Filtros/Filtros.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
 import styles from "./Filtros.module.css";
 
 export default function Filtros({ onTop, onTemporada }) {
   return (
-    <>
-      <button onClick={onTop}>Top Animes</button>
-      <button onClick={onTemporada}>Temporada</button>
-    </>
+    <div className={styles.container}>
+      <button className={styles.botoes} onClick={onTop}>
+        Top Animes
+      </button>
+      <button className={styles.botoes} onClick={onTemporada}>
+        Temporada
+      </button>
+    </div>
   );
 }
